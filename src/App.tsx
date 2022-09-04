0b59e65fca203f89793d88dda63374292d65fe9a
thayron matos <thayron.matos@soluti.com.br>
2022-09-04 01:54:06 -0300

    Componente: Summary

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { ThemeProvider } from "styled-components";
 import { Transactions } from "./pages/Transactions";
 import { GlobalStyle } from "./styles/global";
+
 import { defaultTheme } from "./styles/themes/default";
 
 export function App() {
